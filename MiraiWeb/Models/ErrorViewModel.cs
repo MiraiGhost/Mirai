4fe26e8ec713604e938ab68ad87d06e4d6b14a52
ya142 <ya142@DESKTOP-OCD55P8>
2023-03-01 23:24:28 +0800

    添加项目文件。

---
+++ MiraiWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiraiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
